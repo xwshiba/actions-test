ad696b32217abdfab30987eeba4ce91a7e7f6dfb
Xinyi Wu <xw2357@columbia.edu>
2022-10-13 16:42:37 -0400

    revise threshold

--- .github/workflows/pdf_diff_check.py
+++ .github/workflows/pdf_diff_check.py
@@ -6,6 +6,7 @@ import pathlib
 
 pdf_1 = sys.argv[1]
 pdf_2 = sys.argv[2]
+t = sys.argv[3]
 
 pathlib.Path(__file__).parents[2].joinpath(
     'docs', '_build', 'pdf', 'diffpdf').mkdir(parents=True, exist_ok=True)
@@ -14,9 +15,9 @@ p = pathlib.Path(__file__).parents[2].joinpath(
     'docs', '_build', 'pdf', 'diffpdf')
 
 
-def log_pdf_diff(pdf_1, pdf_2, dir):
+def log_pdf_diff(pdf_1, pdf_2, dir, threshold):
 
-    pdf_similar(pdf_1, pdf_2, tempdir=dir, threshold=50)
+    pdf_similar(pdf_1, pdf_2, tempdir=dir, threshold=threshold)
 
 
-log_pdf_diff(pdf_1, pdf_2, p)
+log_pdf_diff(pdf_1, pdf_2, p, t)

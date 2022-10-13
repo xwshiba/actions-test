39ede1f769b3f1ab67f3b8e2ecbde78b22d061b4
Xinyi Wu <xw2357@columbia.edu>
2022-10-13 15:09:29 -0400

    try pdf_diff_check.py

--- .github/workflows/pdf_diff_check.py
+++ .github/workflows/pdf_diff_check.py
@@ -6,8 +6,6 @@ import pathlib
 
 pdf_1 = sys.argv[1]
 pdf_2 = sys.argv[2]
-print(pdf_1)
-print(pdf_2)
 
 pathlib.Path(__file__).parents[2].joinpath(
     'docs', '_build', 'pdf', 'diffpdf').mkdir(parents=True, exist_ok=True)
